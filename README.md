c2a5c81ea699b2dd4d5900e4fcc879a8bfeae200
tzipiKlar <t0527612265@gmail.com>
2025-07-16 12:25:14 +0300

    📝Update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Web Api לניהול משתמשים ופריטים | ASP .NET Core
+# Web Api לניהול משתמשים ופריטים | ASP .NET Core #
+
 פיתוח מערכת  API לניהול משתמשים ופריטים ב- ASP.NET Core עם JWT  להרשאה, הצפנת סיסמאות וניהול לוגים ב-.Serilog  יישום עקרונות  SOLID, Dependency Injection ו-Middleware  מותאם ליצירת קוד מודולרי ותחזוקתי.
--- obj/Debug/net8.0/CoreProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/CoreProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d49647606efa3a9d16bf45c7bdf75c38369028e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f301556f81a0b45313cb21f9f78a52d425cded")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
